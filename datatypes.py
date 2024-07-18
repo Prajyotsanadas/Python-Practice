de2281058dc3061f916c099df8d2c3d2ebffcb85
Dell <prajotsanadas119@gmail.com>
2024-07-18 08:23:28 +0545

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+# # #There are these following datatyes in Python
+# # Integer : int (1, 10, 100)
+# # Floating point: float (1.0, 10.0, 100.0)
+# # Strings : str ('hello', "Hello", '10')
+# # Lists : list  ([10, "hello", 200.3])
+# # Dictionaries: dict ({"mykey":"value","name":"Sam"})
+# # Tuples : tup ((10, "hello", 200.3))
+# # Sets : set ({"a", "b"})
+# # Booleans :bool (True or False)
+
+
+
